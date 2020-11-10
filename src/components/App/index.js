bb5fccee76523e55a2b958e8bfd220c50dfbc0e3
Dana <leva.daniela@yahoo.com>
2020-11-10 12:33:17 +0000

    using fetch

--- src/components/App/index.js
+++ src/components/App/index.js
@@ -3,27 +3,27 @@ import React, { useState } from "react";
 import Quotes from "../Quotes/index";
 
 function App() {
-  // const [id, setId] = useState("");
+  const [id, setId] = useState("");
 
-  // function getRandomId() {
-  //   let randomId = Math.floor(Math.random() * 10) + 1;
-  //   console.log(randomId);
-  //   setId(randomId);
-  // }
+  function getRandomId() {
+    let randomId = Math.floor(Math.random() * 10) + 1;
+    console.log(randomId);
+    setId(randomId);
+  }
 
   return (
     <div className="App">
       {/* <header className="App-header">
         <p>Happy Project Week</p>
       </header> */}
-        {/* <button
-          onClick={() => {
-            getRandomId();
-          }}
-        >
-          Inspire Me
-        </button> */}
-        <Quotes />
+      <button
+        onClick={() => {
+          getRandomId();
+        }}
+      >
+        Inspire Me
+      </button>
+      <Quotes id={id} />
     </div>
   );
 }
