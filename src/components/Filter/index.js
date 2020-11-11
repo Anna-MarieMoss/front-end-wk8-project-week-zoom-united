c07d42e9d88197c613ac7946c370f2a1922c9edc
Anna-Marie Moss <ambrown1976@googlemail.com>
2020-11-11 16:29:49 +0000

    homepage input

--- src/components/Filter/index.js
+++ src/components/Filter/index.js
@@ -1,10 +1,12 @@
 import React, { useState } from "react";
+
 // import "./input.css";
 
 function Filter({ setQuery }) {
   const [memberId, setMemberId] = useState("");
   return (
     <div className="input-container">
+      <h2>Please enter your Student ID Number</h2>
       <input value={memberId} onChange={(e) => setMemberId(e.target.value)}></input>
 
       <button onClick={() => setQuery(memberId)}>click me</button>
